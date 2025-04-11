6d3b50a045e1ea4ce4dbe7dbc956c5210dae67c5
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-04-11 20:58:51 +0100

    express-static-files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 // Your code here
+
+app.use('scripts', express.static(path.join(__dirname, 'assets/scripts')));
+app.use('/stylesheets', express.static(path.join(__dirname, 'assets/css')));
+app.use('/stickers', express.static(path.join(__dirname, 'assets/images')));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
